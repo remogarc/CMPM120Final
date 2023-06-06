a02e1d696327f827e423a229a31ebcc57e9b0c17
Reese Garcia <remogarc@ucsc.edu>
2023-06-05 21:05:00 -0700

    Implemented music toggle

--- CoreGamplay/JS/game.js
+++ CoreGamplay/JS/game.js
@@ -328,6 +328,9 @@ class LevelOne extends ConfigureScene {
         }
         window.addEventListener('resize', resizeBackground);
         resizeBackground();
+        this.toggleMute();
+
+        // this.add.text(50, 50, 'Cosmic Cleanup', { fontSize: 70 } )
         // Create the platform
         // (scene, x, y, width, height, color) - param to pass for platform
         // Three main platforms for the game 
@@ -427,6 +430,7 @@ class LevelTwo extends ConfigureScene {
       }
       window.addEventListener('resize', resizeBackground);
       resizeBackground();
+      this.toggleMute();
 
       // add platforms
       const platform = new Platform(this,gameWidth * .05,gameHeight * .73, gameWidth * 0.1, gameHeight * 0.5, 0xD2B48C);
@@ -565,6 +569,7 @@ class LevelThree extends ConfigureScene {
     }
     window.addEventListener('resize', resizeBackground);
     resizeBackground();
+    this.toggleMute();
 
     // Create the platforms
     const platform = new Platform(this,gameWidth * .1,gameHeight * .94, gameWidth * 0.2, gameHeight * 0.1, 0x8AC1FA);
